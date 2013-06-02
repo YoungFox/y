c28e1a30b850ce88ffb9b4589255949ca956cbf3
yangwenliang <yang6233562@126.com>
2013-06-02 22:32:53 +0800

    'aa'

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ logo地址
 http://g.hiphotos.baidu.com/album/w%3D1600%3Bq%3D90/sign=adf3bf77f636afc30e0c3b638329d0b5/caef76094b36acaf8061644b7dd98d1000e99c80.jpg
 
 
-openshift地址：http://firstapp-yangwl.rhcloud.com/
+openshift地址：https://firstapp-yangwl.rhcloud.com/
---
+++ public/javascripts/base.js
@@ -0,0 +1,22 @@
+
+//用户名校验
+function checkName(strName){
+	var reg=/^[a-z0-9_\u4e00-\u9fa5]{3,12}$/i;
+	if(reg.test(strName)){
+		return true;
+		}
+		else{
+			return false;
+		}
+}
+
+//邮箱校验
+function isEmail(strEmail) {
+	var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+	if (reg.test(strEmail)){
+		return true;
+	}
+	else{
+		return false;
+	  }
+}
